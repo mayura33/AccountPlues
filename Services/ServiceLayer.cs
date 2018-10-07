c20a9dd19647dec24b700722d468f952a563e74a
Mayur Asodariya <mayurasodariya33@gmail.com>
2018-10-07 17:13:44 +0530

    Sells Page completed

--- Services/ServiceLayer.cs
+++ Services/ServiceLayer.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Data_Models;
+using DataRepository;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +12,28 @@ namespace Services
 {
     public class ServiceLayer
     {
+        public bool CheckIsUserValid(LoginUser login)
+        {
+            Repository repository = new Repository();
+            return repository.checkIsUserValid(login);
+        }
 
-    }
+		public bool InsertStock(PurchaseDetails purchaseDetails)
+		{
+			Repository repository = new Repository();
+			return repository.InsertStock(purchaseDetails);
+		}
+
+		public bool InsertSells(SellsDetail sellsDetail)
+		{
+			Repository repository = new Repository();
+			return repository.InsertSells(sellsDetail);
+		}
+
+		public DataTable GetPurchaseDetails()
+		{
+			Repository repository = new Repository();
+			return repository.GetPurchaseDetails();
+		}
+	}
 }
